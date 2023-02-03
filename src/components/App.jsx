a4d4890ce3b1aae94a566323d4be7065dc5d4458
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-03 10:37:19 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { PageTitle } from 'components/PageTitle/PageTitle';
+import { EventBoard } from 'components/EventBoard/EventBoard';
+import upcomingEvents from '../upcoming-events.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <PageTitle text="24th Core Worlds Coalition Conference" />
+      <EventBoard events={upcomingEvents} />
+    </>
   );
 };
